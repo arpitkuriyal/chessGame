6a5bbea3c1dd03c413aa6abf64aee1ec2e5c97ce
Arpit Kuriyal <arpitkuriyal2002@gmail.com>
2024-12-07 16:23:09 +0530

    fixed some issue in the frontend folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,17 @@
-import { useEffect, useState } from 'react'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Landing from './pages/Landing'
+import Game from './pages/Game'
 
 function App() {
-  const [input,setInput] = useState<string>("")
-  const [messages,setMessages]=useState<string[]>([])
-  const[socket,setSocket]=useState<WebSocket | null>(null)
-
-  useEffect(()=>{
-    const socket =new WebSocket("ws://localhost:8080")
-    setSocket(socket)
-
-    socket.onopen=()=>{
-      console.log("connection started")
-    }
-    socket.onmessage=(event)=>{
-      setMessages((prev)=>[...prev,event.data])
-    }
-    socket.onerror = (error) => console.error("WebSocket error:", error);
-    socket.onclose = () => console.log("WebSocket closed!");
-    },[])
-    const sendMessages=(messages: string)=>{
-      if(socket?.readyState===WebSocket.OPEN){
-        socket.send(messages)
-      }
-    }
-    console.log(messages)
-
   return (
-    <div>
-      <input onChange={(e)=>{
-        setInput(e.target.value)
-      }} placeholder='write your message here'
-      type='text'
-      value={input}>
-
-      </input>
-      <button onClick={()=>{sendMessages(input),setInput('')}}>send</button>
-      <ul>
-      {messages.map((msg,index)=>{
-        return <li key={index}>{msg}</li>
-      })}
-      </ul>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/Game' element={<Game/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
