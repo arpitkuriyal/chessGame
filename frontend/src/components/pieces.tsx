b994b3370546ea896c8aa075eecc85f5cbc64080
Arpit Kuriyal <arpitkuriyal2002@gmail.com>
2026-03-27 18:03:32 +0530

    temporary hack to turn pieces to black

--- frontend/src/components/pieces.tsx
+++ frontend/src/components/pieces.tsx
@@ -12,16 +12,16 @@ import nw from '../assets/nw.svg'
 import nb from '../assets/nb.svg'
 
 export const unicodePieces: { [key: string]: JSX.Element } = {
-  pw: <img src={pw} alt="White Pawn"/>,  
+  pw: <img src={pw} alt="White Pawn" />,  
   rw: <img src={rw} alt="White Rook" />,   
   nw: <img src={nw} alt="White Knight" />, 
   bw: <img src={bw} alt="White Bishop" />, 
   qw: <img src={qw} alt="White Queen" />, 
   kw: <img src={kw} alt="White King" />,   
-  pb: <img src={pb} alt="Black Pawn" />,   
-  rb: <img src={rb} alt="Black Rook" />,   
-  nb: <img src={nb} alt="Black Knight" />, 
-  bb: <img src={bb} alt="Black Bishop" />, 
-  qb: <img src={qb} alt="Black Queen" />, 
-  kb: <img src={kb} alt="Black King" />,   
+  pb: <img src={pb} alt="Black Pawn" style={{ filter: "invert(1)" }}/>,   
+  rb: <img src={rb} alt="Black Rook" style={{ filter: "invert(1)" }}/>,   
+  nb: <img src={nb} alt="Black Knight" style={{ filter: "invert(1)" }}/>, 
+  bb: <img src={bb} alt="Black Bishop" style={{ filter: "invert(1)" }}/>, 
+  qb: <img src={qb} alt="Black Queen" style={{ filter: "invert(1)" }}/>, 
+  kb: <img src={kb} alt="Black King" style={{ filter: "invert(1)" }}/>,   
 };
